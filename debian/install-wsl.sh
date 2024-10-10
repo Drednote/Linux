c61792cb884b9ed2a9e8d162a24f868555f3f9ab
Ivan Galushko <galushko.ivan8@gmail.com>
2024-10-10 17:26:03 +0300

    fix install.sh

--- debian/install-wsl.sh
+++ debian/install-wsl.sh
@@ -6,13 +6,9 @@ YELLOW='\033[1;33m'
 WHITE='\033[1;37m'
 NC='\033[0m' # No Color
 
-UTILS="curl vim htop git"
+UTILS="vim htop"
 
-# shellcheck disable=SC2086
-sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install $UTILS
-
-git config --global user.name "Ivan Galushko"
-git config --global user.email "galushko.ivan8@gmail.com"
+sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y install curl
 
 CUR_PWD=$(pwd)
 mkdir "$HOME"/bin
@@ -20,7 +16,7 @@ cp -r "$CUR_PWD"/bin "$HOME"
 chmod +x "$HOME"/bin/open
 chmod +x "$HOME"/bin/my
 mkdir "$HOME"/IdeaProjects
-ln -s "$HOME"/IdeaProjects/docker-compose "$HOME"/bin/docker-compose
+ln -s "$HOME"/bin/docker-compose "$HOME"/IdeaProjects/docker-compose
 
 read -p "$(echo -e ${YELLOW}"==>${WHITE}Install $UTILS? (y,n) ${NC}")" UT
 if [[ $UT == "Y" || $UT == "y" ]]; then
@@ -37,12 +33,6 @@ read -p "$(echo -e ${YELLOW}"==>${WHITE}Configure Sdkman? (y,n) ${NC}")" SDK
 if [[ $SDK == "Y" || $SDK == "y" ]]; then
     sudo apt-get -y install unzip zip
     curl -s "https://get.sdkman.io" | bash
-fi
-
-read -p "$(echo -e ${YELLOW}"==>${WHITE}Configure k3s? (y,n) ${NC}")" k3c
-if [[ $k3c == "Y" || $k3c == "y" ]]; then
-    sudo apt-get -y install systemd dbus dbus-user-session
-    curl -sfL https://get.k3s.io | sh -
 fi
 
 ### Script is done ###
